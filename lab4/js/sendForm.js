1a6ae06337144bb050286d3dbac145dcacf9a839
Daniil Nizovkin <dan_nizovkin_kungur@mail.ru>
2019-04-02 18:23:20 +0300

    Implement the posts sending.
    Signed-off-by: Daniil Nizovkin <dan_nizovkin_kungur@mail.ru>

---
+++ lab4/js/sendForm.js
@@ -0,0 +1,25 @@
+let form = document.getElementById('addPost');
+
+form.onsubmit = (e) => {
+    e.preventDefault();
+
+    var username = document.getElementById('username').value,
+        title = document.getElementById('title').value,
+        postBody = document.getElementById('body').value,
+
+        requestBody = "username=" + encodeURIComponent(username) +
+            "&title=" + encodeURIComponent(title) +
+            "&body=" + encodeURIComponent(postBody),
+        xhr = new XMLHttpRequest();
+    xhr.open('POST', 'http://localhost:8081/slimapp/public/api/news/add');
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send(requestBody);
+
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            form.reset();
+            // alert("The post has been submitted for review!");
+            console.log(xhr.responseText)
+        }
+    }
+};
